5abd28e1a8034c5438f6bee0b2607f40d641021a
Dario Djuric <dario.djuric@altima.hr>
2021-01-04 22:57:36 +0100

    Add support for multiple streams

--- pcap2ascii.py
+++ pcap2ascii.py
@@ -3,14 +3,21 @@ import sys
 
 
 def main(tshark_path, pcap_path):
-    # https://osqa-ask.wireshark.org/questions/56164/extract-payload-from-tcp-stream
+    session_list_lines = subprocess.run([tshark_path, "-r", pcap_path, "-Y", "usb", "-z", "conv,tcp"],
+                                        stdout=subprocess.PIPE).stdout.decode().splitlines()
 
-    result = subprocess.run([tshark_path, "-r", pcap_path, "-Y", "us", "-z", "follow,tcp,ascii,{}".format(0)],
-                            stdout=subprocess.PIPE)
-    result_lines = result.stdout.decode().splitlines()
-    lines_without_header = result_lines[result_lines.index("Follow: tcp,ascii") + 4:]
-    tcp_stream_ascii = "".join([lines_without_header[i] for i in range(len(lines_without_header)) if i % 2 == 1])
-    print(tcp_stream_ascii)
+    stream_count = len(session_list_lines) - 6  # Minus headers and footers
+
+    for stream_id in range(stream_count):
+        result = subprocess.run(
+            [tshark_path, "-r", pcap_path, "-Y", "usb", "-z", "follow,tcp,ascii,{}".format(stream_id)],
+            stdout=subprocess.PIPE)
+        result_lines = result.stdout.decode().splitlines()
+        lines_without_header = result_lines[result_lines.index("Follow: tcp,ascii") + 4:]
+        tcp_stream_ascii = "".join([lines_without_header[i] for i in range(len(lines_without_header)) if i % 2 == 1])
+
+        with open("stream-{}.log".format(stream_id), "a") as result_file:
+            result_file.write(tcp_stream_ascii)
 
 
 main(sys.argv[1], sys.argv[2])
