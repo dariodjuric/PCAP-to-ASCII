a36cd6b3f6477a62cfbf25204c6c5c7222ea1541
Dario Djuric <dario.djuric@altima.hr>
2021-01-04 22:45:05 +0100

    Add logic for parsing TCP dump

--- pcap2ascii.py
+++ pcap2ascii.py
@@ -5,9 +5,12 @@ import sys
 def main(tshark_path, pcap_path):
     # https://osqa-ask.wireshark.org/questions/56164/extract-payload-from-tcp-stream
 
-    result = subprocess.run([tshark_path, "-r", pcap_path, "-Y", "usb", "-z", "follow,tcp,ascii,0"],
+    result = subprocess.run([tshark_path, "-r", pcap_path, "-Y", "us", "-z", "follow,tcp,ascii,{}".format(0)],
                             stdout=subprocess.PIPE)
-    print(result.stdout.decode())
+    result_lines = result.stdout.decode().splitlines()
+    lines_without_header = result_lines[result_lines.index("Follow: tcp,ascii") + 4:]
+    tcp_stream_ascii = "".join([lines_without_header[i] for i in range(len(lines_without_header)) if i % 2 == 1])
+    print(tcp_stream_ascii)
 
 
 main(sys.argv[1], sys.argv[2])
